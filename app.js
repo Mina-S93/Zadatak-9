14739cc6f9cdeca515f31e0ee549ef8f0e232f66
Mina-S93 <jasminai93@gmail.com>
2021-11-21 13:52:29 +0100

    styling remove btn

--- app.js
+++ app.js
@@ -80,7 +80,7 @@ function addItemToCart(title, price, imageSrc) {
         <span class="cart-price cart-column">${price}</span>
         <div class="cart-quantity cart-column">
             <input class="cart-quantity-input" type="number" value="1">
-            <button class="btn btn-danger" type="button">REMOVE</button>
+            <button class="btn-danger" type="button">REMOVE</button>
         </div>`;
 	cartRow.innerHTML = cartRowContents;
 	cartItems.append(cartRow);
